a1680cafbf7a6e3769fb1ff3bbf66c986c662d87
duongnghoang <hoangduongatwork@gmail.com>
2025-04-15 19:53:52 +0700

    add unit test

---
+++ PersonMVC.Test/Controllers/PersonControllerTest.cs
@@ -0,0 +1,410 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using PersonMVC.Common;
+using PersonMVC.Controllers;
+using PersonMVC.Responses;
+using PersonMVC.Services;
+using PersonMVC.ViewModels.Persons;
+
+namespace PersonMVC.Test.Controllers;
+
+[TestFixture]
+public class PersonControllerTest
+{
+    private Mock<IPersonService> _personServiceMock;
+    private PersonController _personController;
+    private const int PAGE_INDEX = 1;
+    private const int PAGE_SIZE = 10;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        // Initialize the mock service
+        _personServiceMock = new Mock<IPersonService>();
+        _personController = new PersonController(_personServiceMock.Object);
+    }
+
+    [Test]
+    public void GetMale_ValidRequest_ReturnsViewWithPaginatedListMalePersons()
+    {
+        // Arrange
+        var paginatedList = new PaginatedList<ListPersonViewModel>([], 0, PAGE_INDEX, PAGE_SIZE);
+
+        _personServiceMock
+            .Setup(x => x.GetPersonsMale(PAGE_INDEX, PAGE_SIZE))
+            .Returns(paginatedList);
+
+        // Act
+        var result = _personController.GetMale(PAGE_INDEX, PAGE_SIZE) as ViewResult;
+
+        // Assert
+        _personServiceMock.Verify(x => x.GetPersonsMale(PAGE_INDEX, PAGE_SIZE), Times.Once);
+        
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        { 
+            Assert.That(result.ViewName, Is.EqualTo("Index"));
+            Assert.That(_personController.ViewBag.Action, Is.Not.Null.And.EqualTo("GetMale"));
+            Assert.That(result.Model, Is.EqualTo(paginatedList));
+        });
+    }
+
+    [Test]
+    public void Index_ValidRequest_ReturnsViewWithPaginatedListPersons()
+    {
+        // Arrange
+        var paginatedList = new PaginatedList<ListPersonViewModel>([], 0, PAGE_INDEX, PAGE_SIZE);
+        _personServiceMock
+            .Setup(x => x.GetAllPersons(PAGE_INDEX, PAGE_SIZE))
+            .Returns(paginatedList);
+
+        // Act
+        var result = _personController.Index(PAGE_INDEX, PAGE_SIZE) as ViewResult;
+
+        // Assert
+        _personServiceMock.Verify(x => x.GetAllPersons(PAGE_INDEX, PAGE_SIZE), Times.Once);
+
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.ViewName, Is.EqualTo("Index"));
+            Assert.That(_personController.ViewBag.Action, Is.Not.Null.And.EqualTo("Index"));
+            Assert.That(result.Model, Is.EqualTo(paginatedList));
+        });
+    }
+
+    [Test]
+    public void GetPersonsByBirthYearAction_IfActionEmpty_ReturnsBadRequestWithMessage()
+    {
+        // Act
+        var result = _personController.GetPersonsByBirthYearAction("", PAGE_INDEX, PAGE_SIZE) as BadRequestObjectResult;
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.StatusCode, Is.EqualTo(StatusCodes.Status400BadRequest));
+            Assert.That(result.Value, Is.EqualTo("Action is required"));
+        });
+    }
+
+    [TestCase("getbirthyearlower")]
+    [TestCase("GetBirthYearLower")]
+    public void GetPersonsByBirthYearAction_IfActionGetBirthYearLower_ReturnsRedirectToActionResult(string action)
+    {
+        // Act
+        var result = _personController.GetPersonsByBirthYearAction(action, PAGE_INDEX, PAGE_SIZE) as RedirectToActionResult;
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.RouteValues, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.ActionName, Is.EqualTo("GetBirthYearLower"));
+            Assert.That(result.RouteValues["pageIndex"], Is.EqualTo(PAGE_INDEX));
+            Assert.That(result.RouteValues["pageSize"], Is.EqualTo(PAGE_SIZE));
+        });
+    }
+
+    [TestCase("getbirthyeargreater")]
+    [TestCase("GetBirthYearGreater")]
+    public void GetPersonsByBirthYearAction_IfActionGetBirthYearGreater_ReturnsRedirectToActionResult(string action)
+    {
+        // Act
+        var result = _personController.GetPersonsByBirthYearAction(action, PAGE_INDEX, PAGE_SIZE) as RedirectToActionResult;
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.RouteValues, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.ActionName, Is.EqualTo("GetBirthYearGreater"));
+            Assert.That(result.RouteValues["pageIndex"], Is.EqualTo(PAGE_INDEX));
+            Assert.That(result.RouteValues["pageSize"], Is.EqualTo(PAGE_SIZE));
+        });
+    }
+
+    [TestCase("getbirthyearequal")]
+    [TestCase("GetBirthYearEqual")]
+    public void GetPersonsByBirthYearAction_IfActionGetBirthYearEqual_ReturnsRedirectToActionResult(string action)
+    {
+        // Act
+        var result = _personController.GetPersonsByBirthYearAction(action, PAGE_INDEX, PAGE_SIZE) as RedirectToActionResult;
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.RouteValues, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.ActionName, Is.EqualTo("GetBirthYearEqual"));
+            Assert.That(result.RouteValues["pageIndex"], Is.EqualTo(PAGE_INDEX));
+            Assert.That(result.RouteValues["pageSize"], Is.EqualTo(PAGE_SIZE));
+        });
+    }
+
+    [TestCase("invalidaction")]
+    public void GetPersonsByBirthYearAction_IfActionInvalid_ReturnsNotFoundWithMessage(string action)
+    {
+        // Act
+        var result = _personController.GetPersonsByBirthYearAction(action, PAGE_INDEX, PAGE_SIZE) as NotFoundObjectResult;
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.StatusCode, Is.EqualTo(StatusCodes.Status404NotFound));
+            Assert.That(result.Value, Is.EqualTo("Action not found"));
+        });
+    }
+
+    [Test]
+    public void GetBirthYearLower_ValidRequest_ReturnsViewWithPaginatedListPersons()
+    {
+        // Arrange
+        var paginatedList = new PaginatedList<ListPersonViewModel>([], 0, PAGE_INDEX, PAGE_SIZE);
+        _personServiceMock
+            .Setup(x => x.GetPersonsByBirthYearWithAction(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>()))
+            .Returns(paginatedList);
+
+        // Act
+        var result = _personController.GetBirthYearLower(PAGE_SIZE, PAGE_INDEX) as ViewResult;
+
+        // Assert
+        _personServiceMock.Verify(x => x.GetPersonsByBirthYearWithAction("lower", PAGE_INDEX, PAGE_SIZE), Times.Once);
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.ViewName, Is.EqualTo("Index"));
+            Assert.That(_personController.ViewBag.Action, Is.Not.Null.And.EqualTo("BirthYearLower"));
+            Assert.That(result.Model, Is.EqualTo(paginatedList));
+        });
+    }
+
+    [Test]
+    public void GetBirthYearGreater_ValidRequest_ReturnsViewWithPaginatedListPersons()
+    {
+        // Arrange
+        var paginatedList = new PaginatedList<ListPersonViewModel>([], 0, PAGE_INDEX, PAGE_SIZE);
+        _personServiceMock
+            .Setup(x => x.GetPersonsByBirthYearWithAction(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>()))
+            .Returns(paginatedList);
+
+        // Act
+        var result = _personController.GetBirthYearGreater(PAGE_SIZE, PAGE_INDEX) as ViewResult;
+
+        // Assert
+        _personServiceMock.Verify(x => x.GetPersonsByBirthYearWithAction("greater", PAGE_INDEX, PAGE_SIZE), Times.Once);
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.ViewName, Is.EqualTo("Index"));
+            Assert.That(_personController.ViewBag.Action, Is.Not.Null.And.EqualTo("BirthYearGreater"));
+            Assert.That(result.Model, Is.EqualTo(paginatedList));
+        });
+    }
+
+    [Test]
+    public void GetBirthYearEqual_ValidRequest_ReturnsViewWithPaginatedListPersons()
+    {
+        // Arrange
+        var paginatedList = new PaginatedList<ListPersonViewModel>([], 0, PAGE_INDEX, PAGE_SIZE);
+        _personServiceMock
+            .Setup(x => x.GetPersonsByBirthYearWithAction(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>()))
+            .Returns(paginatedList);
+
+        // Act
+        var result = _personController.GetBirthYearEqual(PAGE_SIZE, PAGE_INDEX) as ViewResult;
+
+        // Assert
+        _personServiceMock.Verify(x => x.GetPersonsByBirthYearWithAction("equal", PAGE_INDEX, PAGE_SIZE), Times.Once);
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.ViewName, Is.EqualTo("Index"));
+            Assert.That(_personController.ViewBag.Action, Is.Not.Null.And.EqualTo("BirthYearEqual"));
+            Assert.That(result.Model, Is.EqualTo(paginatedList));
+        });
+    }
+
+    [Test]
+    public void Create_Get_ReturnsPersonFormView()
+    {
+        // Act
+        var result = _personController.Create() as ViewResult;
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.ViewName, Is.EqualTo("PersonForm"));
+            Assert.That(result.ViewData["Action"], Is.EqualTo("Create"));
+        });
+    }
+
+    [Test]
+    public void Create_PostIfModelStateIsValid_ReturnsRedirectToAction()
+    {
+        // Arrange
+        var person = new FormPersonViewModel();
+        _personServiceMock.Setup(x => x.Add(person)).Verifiable();
+
+        // Act
+        var result = _personController.Create(person) as RedirectToActionResult;
+
+        // Assert
+        _personServiceMock.Verify(x => x.Add(person), Times.Once);
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.ActionName, Is.EqualTo("Index"));
+    }
+
+    [TestCase(1)]
+    public void Detail_IfPersonIdExist_ReturnsDetailView(int id)
+    {
+        // Arrange
+        var personDetail = new DetailPersonViewModel { Id = 1 };
+        _personServiceMock.Setup(x => x.GetPersonDetail(id))
+            .Returns(personDetail);
+
+        // Act
+        var result = _personController.Detail(id) as ViewResult;
+
+        // Assert
+        _personServiceMock.Verify(x => x.GetPersonDetail(id), Times.Once);
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Model, Is.EqualTo(personDetail));
+        });
+    }
+
+    [TestCase(100)]
+    public void Detail_IfPersonIdNotExist_ThrowsException(int id)
+    {
+        // Arrange
+        _personServiceMock.Setup(x => x.GetPersonDetail(id))
+            .Throws<InvalidOperationException>();
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => _personController.Detail(id));
+    }
+
+    [Test]
+    public void DeleteConfirmation_Get_ReturnsView()
+    {
+        // Act
+        var result = _personController.DeleteConfirmation() as ViewResult;
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.ViewName, Is.EqualTo("DeleteConfirmation"));
+    }
+
+    [TestCase(1)]
+    public void Delete_PostIfPersonIdExist_ReturnsDeleteConfirmationView(int id)
+    {
+        // Arrange
+        var tempData = new TempDataDictionary(new DefaultHttpContext(), Mock.Of<ITempDataProvider>());
+        _personController.TempData = tempData;
+        const string DELETED_PERSON_NAME = "Deleted name";
+        _personServiceMock.Setup(x => x.Delete(1))
+            .Returns(DELETED_PERSON_NAME);
+        const string DELETED_NAME_SUCCESS_MESSAGE = $"The person {DELETED_PERSON_NAME} was deleted successfully.";
+
+        // Act
+        var result = _personController.Delete(1) as RedirectToActionResult;
+
+        // Assert
+        _personServiceMock.Verify(x => x.Delete(1), Times.Once);
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.ActionName, Is.EqualTo("DeleteConfirmation"));
+            Assert.That(_personController.TempData["SuccessMessage"],
+                Is.Not.Null.And.EqualTo(DELETED_NAME_SUCCESS_MESSAGE));
+        });
+    }
+
+    [TestCase(1)]
+    public void Edit_GetIfPersonIdExist_ReturnsPersonFormView(int id)
+    {
+        // Arrange
+        var personDetail = new FormPersonViewModel { Id = 1 };
+        _personServiceMock.Setup(x => x.GetPersonDetailForm(id))
+            .Returns(personDetail);
+
+        // Act
+        var result = _personController.Edit(id) as ViewResult;
+
+        // Assert
+        _personServiceMock.Verify(x => x.GetPersonDetailForm(id), Times.Once);
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.ViewName, Is.EqualTo("PersonForm"));
+            Assert.That(_personController.ViewBag.Action, Is.Not.Null.And.EqualTo("Edit"));
+            Assert.That(result.Model, Is.EqualTo(personDetail));
+        });
+    }
+
+    [TestCase(100)]
+    public void Edit_GetIfPersonIdNotExist_ThrowsException(int id)
+    {
+        // Arrange
+        _personServiceMock.Setup(x => x.GetPersonDetailForm(id))
+            .Throws<InvalidOperationException>();
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => _personController.Edit(id));
+        _personServiceMock.Verify(x => x.GetPersonDetailForm(id), Times.Once);
+    }
+
+    [Test]
+    public void Edit_PostIfModelStateIsValid_ReturnsRedirectToAction()
+    {
+        // Arrange
+        var person = new FormPersonViewModel();
+        _personServiceMock.Setup(x => x.Update(person)).Verifiable();
+
+        // Act
+        var result = _personController.Edit(person) as RedirectToActionResult;
+
+        // Assert
+        _personServiceMock.Verify(x => x.Update(person), Times.Once);
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.ActionName, Is.EqualTo("Index"));
+    }
+
+    [Test]
+    public void ExportToExcel_ReturnsFileResponse()
+    {
+        // Arrange
+
+        var expectedBytes = new byte[] { 1, 2, 3 };
+        const string EXPECTED_CONTENT_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+        const string EXPECTED_FILENAME = "Persons_20250415120000.xlsx";
+        var fileResponse = new FileResponse(expectedBytes, EXPECTED_CONTENT_TYPE, EXPECTED_FILENAME);
+        _personServiceMock.Setup(x => x.ExportToExcel())
+            .Returns(fileResponse);
+        
+        // Act
+        var result = _personController.ExportToExcel() as FileContentResult;
+
+        // Assert
+        _personServiceMock.Verify(x => x.ExportToExcel(), Times.Once);
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.FileDownloadName, Is.EqualTo(fileResponse.FileName));
+            Assert.That(result.FileContents, Is.EqualTo(fileResponse.FileContent));
+            Assert.That(result.ContentType, Is.EqualTo(fileResponse.ContentType));
+        });
+    }
+
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        _personController.Dispose();
+    }
+}
---
+++ PersonMVC.Test/Services/PersonServiceTests.cs
@@ -0,0 +1,323 @@
+﻿using PersonMVC.Common.Data;
+using PersonMVC.Common.Enums;
+using PersonMVC.Models;
+using PersonMVC.Responses;
+using PersonMVC.Services;
+using PersonMVC.ViewModels.Persons;
+
+namespace PersonMVC.Test.Services;
+
+[TestFixture]
+public class PersonServiceTests
+{
+    private PersonService _personService;
+    private const int PAGE_INDEX = 1;
+    private const int PAGE_SIZE = 10;
+
+    [SetUp]
+    public void Setup()
+    {
+        // Setup code here
+        var personData = new PersonData
+        {
+            Persons =
+            [
+                new Person(1, "ABC", "Doe", Gender.Male, new DateTime(1999, 1, 21), "123-654-8799", "New York", true),
+                new Person(2, "DEF", "Doe", Gender.Female, new DateTime(2001, 12, 2), "254-367-9801", "Los Angeles", false),
+                new Person(3, "GHI", "Smith", Gender.Male, new DateTime(1995, 6, 15), "345-678-1234", "Chicago", true),
+                new Person(4, "JKL", "Johnson", Gender.Female, new DateTime(1998, 10, 30), "456-789-2345", "Houston", false),
+                new Person(5, "MNO", "Brown", Gender.Male, new DateTime(2000, 7, 18), "567-890-3456", "Phoenix", true),
+                new Person(6, "PQR", "Davis", Gender.Female, new DateTime(2002, 3, 5), "678-901-4567", "Philadelphia", false),
+                new Person(7, "STU", "Wilson", Gender.Male, new DateTime(1997, 8, 25), "789-012-5678", "San Francisco", true)
+            ]
+        };
+
+        _personService = new PersonService(personData);
+    }
+
+    [Test]
+    public void Add_ValidRequest_ReturnsNewPerson()
+    {
+        // Arrange
+        var personModel = new FormPersonViewModel
+        {
+            FirstName = "Jane",
+            LastName = "Doe",
+            PhoneNumber = "123-456-7890",
+            Gender = Gender.Female,
+            DateOfBirth = new DateTime(1992, 2, 2),
+            BirthPlace = "Los Angeles",
+            IsGraduated = true
+        };
+
+        // Act
+        _personService.Add(personModel);
+        var personList = _personService.GetAllPersons(PAGE_INDEX, PAGE_SIZE);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(personList, Has.Count.EqualTo(8));
+            Assert.That(personList[7].Id, Is.EqualTo(8));
+        });
+    }
+
+    [Test]
+    public void GetAllPersons_ReturnsPaginatedList()
+    {
+        // Act
+        var result = _personService.GetAllPersons(PAGE_INDEX, PAGE_SIZE);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Has.Count.EqualTo(7));
+        });
+    }
+
+    [TestCase(1)]
+    public void Delete_IfPersonIdExist_ReturnsDeletedPersonName(int id)
+    {
+        // Arrange
+        const string EXPECTED_NAME = "ABC Doe";
+
+        // Act
+        var deletedPerson = _personService.Delete(id);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(deletedPerson, Is.EqualTo(EXPECTED_NAME));
+            var personList = _personService.GetAllPersons(PAGE_INDEX, PAGE_SIZE);
+            Assert.That(!personList.Exists(p => p.Id == id));
+        });
+    }
+
+    [TestCase(100)]
+    public void Delete_IfPersonIdNotExist_ThrowsException(int id)
+    {
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => _personService.Delete(id), "No person found with the given ID.");
+    }
+
+    [TestCase(1)]
+    public void Update_IfPersonIdExist_UpdatePersonData(int id)
+    {
+        // Arrange
+        var personModel = new FormPersonViewModel
+        {
+            Id = id,
+            FirstName = "Jane",
+            LastName = "Doe",
+            Gender = Gender.Female,
+            PhoneNumber = "123-456-1234",
+            DateOfBirth = new DateTime(1992, 2, 2),
+            BirthPlace = "Los Angeles",
+            IsGraduated = true
+        };
+
+        // Act
+        _personService.Update(personModel);
+
+        // Assert
+        var updatedPerson = _personService
+            .GetAllPersons(PAGE_INDEX, PAGE_SIZE)
+            .First(p => p.Id == id);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(updatedPerson.FullName, Is.EqualTo("Jane Doe"));
+            Assert.That(updatedPerson.Gender, Is.EqualTo(Gender.Female));
+            Assert.That(updatedPerson.PhoneNumber, Is.EqualTo("123-456-1234"));
+            Assert.That(updatedPerson.DateOfBirth, Is.EqualTo(new DateTime(1992, 2, 2)));
+            Assert.That(updatedPerson.BirthPlace, Is.EqualTo("Los Angeles"));
+            Assert.That(updatedPerson.GraduatedString, Is.EqualTo("Yes"));
+        });
+    }
+
+    [TestCase(100)]
+    public void Update_IfPersonNotExist_ThrowsException(int id)
+    {
+        // Arrange
+        var personModel = new FormPersonViewModel
+        {
+            Id = id,
+        };
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => _personService.Update(personModel),  "No person found with the given ID.");
+    }
+
+    [TestCase("greater")]
+    public void GetPersonByBirthYearWithAction_ActionIsGreater_ReturnsDOBYearGreater2000(string action)
+    {
+        // Act
+        var result = _personService.GetPersonsByBirthYearWithAction(action, PAGE_INDEX, PAGE_SIZE);
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Has.Count.EqualTo(2));
+            Assert.That(result.Select(p => p.DateOfBirth.Year), Is.All.GreaterThan(2000));
+        });
+        
+    }
+
+    [TestCase("lower")]
+    public void GetPersonByBirthYearWithAction_ActionIsLess_ReturnsDOBYearLess2000(string action)
+    {
+        // Act
+        var result = _personService.GetPersonsByBirthYearWithAction(action, PAGE_INDEX, PAGE_SIZE);
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Has.Count.EqualTo(4));
+            Assert.That(result.Select(p => p.DateOfBirth.Year), Is.All.LessThan(2000));
+        });
+    }
+
+    [TestCase("equal")]
+    public void GetPersonByBirthYearWithAction_ActionIsEqual_ReturnsDOBYearEqual2000(string action)
+    {
+        // Act
+        var result = _personService.GetPersonsByBirthYearWithAction(action, PAGE_INDEX, PAGE_SIZE);
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Has.Count.EqualTo(1));
+            Assert.That(result.Select(p => p.DateOfBirth.Year), Is.All.EqualTo(2000));
+        });
+    }
+
+    [TestCase("invalid")]
+    public void GetPersonByBirthYearWithAction_ActionIsNotValid_ReturnsEmptyList(string action)
+    {
+        // Act
+        var result = _personService.GetPersonsByBirthYearWithAction(action, PAGE_INDEX, PAGE_SIZE);
+
+        // Assert
+        Assert.That(result, Has.Count.Zero);
+    }
+
+    [Test]
+    public void GetPersonsMale_ReturnsMalePersons()
+    {
+        // Arrange
+        const string GENDER_PROPERTY = "Gender";
+
+        // Act
+        var result = _personService.GetPersonsMale(PAGE_INDEX, PAGE_SIZE);
+
+        // Assert
+        Assert.That(result, Has.Count.EqualTo(4));
+        Assert.That(result, Has.All.Property(GENDER_PROPERTY).EqualTo(Gender.Male));
+    }
+
+    [Test]
+    public void GetOldestAge_ReturnsOldestPerson()
+    {
+        // Act
+        var result = _personService.GetPersonOldestAge();
+
+        // Assert
+        Assert.That(result.Id, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void ExportToExcel_ReturnsFileResponse()
+    {
+        // Arrange
+        const string EXCEL_FILE_NAME_PREFIX = "Persons_";
+        const string EXCEL_FILE_NAME_EXTENSION = ".xlsx";
+        const string EXCEL_FILE_CONTENT_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+        // Act
+        var result = _personService.ExportToExcel();
+        var fileName = result.FileName;
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.InstanceOf<FileResponse>());
+            Assert.That(fileName, Does.StartWith(EXCEL_FILE_NAME_PREFIX));
+            Assert.That(fileName, Does.EndWith(EXCEL_FILE_NAME_EXTENSION));
+            Assert.That(result.ContentType, Is.EqualTo(EXCEL_FILE_CONTENT_TYPE));
+            Assert.That(result.FileContent, Is.Not.Null);
+        });
+        
+    }
+
+    [TestCase(1)]
+    public void GetPersonDetail_IfPersonIdExist_ReturnsPersonDetail(int id)
+    {
+        // Arrange
+        const string EXPECTED_FIRST_NAME = "ABC";
+
+        // Act
+        var result = _personService.GetPersonDetail(id);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Id, Is.EqualTo(id));
+            Assert.That(result.FirstName, Is.EqualTo(EXPECTED_FIRST_NAME));
+        });
+    }
+
+    [TestCase(100)]
+    public void GetPersonDetail_IfPersonIdNotExist_ThrowsException(int id)
+    {
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => _personService.GetPersonDetail(id), "No person found with the given ID.");
+    }
+
+    [Test]
+    public void GetPersonsFullName_ReturnsFullNames()
+    {
+        // Arrange
+        var expectedFullNames = new List<string>
+        {
+            "ABC Doe",
+            "DEF Doe",
+            "GHI Smith",
+            "JKL Johnson",
+            "MNO Brown",
+            "PQR Davis",
+            "STU Wilson"
+        };
+
+        // Act
+        var result = _personService.GetPersonsFullName();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedFullNames));
+    }
+
+    [TestCase(1)]
+    public void GetPersonDetailForm_IfPersonIdExist_ReturnFormPerson(int id)
+    {
+        // Act
+        var result = _personService.GetPersonDetailForm(1);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Id, Is.EqualTo(1));
+        });
+    }
+
+    [TestCase(100)]
+    public void GetPersonDetailForm_IfPersonIdNotExist_ThrowsException(int id)
+    {
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => _personService.GetPersonDetailForm(id), "No person found with the given ID.");
+    }
+}
